f04002ac7a70ecf03d4f9490d91264cd8ba74575
Matias De Santi <matias.desanti@wolox.com.ar>
2018-04-23 21:40:44 -0300

    Initial commit

--- validation.go
+++ validation.go
@@ -1,105 +1,106 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
 
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
-
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
-
-    HTTP_PORT = "8008"//591, 8000, 8008, 8080
-    CONN_HOST = "localhost"
-
+	HTTP_PORT = "8008" //591, 8000, 8008, 8080
+	CONN_HOST = "localhost"
 )
 
 func main() {
-    
-    go listenHTTP()
 
-    for {}
-}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = HTTP_PORT
+	}
 
-func listenHTTP() {
+	log.WithFields(log.Fields{
+		"host": CONN_HOST,
+		"port": port,
+	}).Info("SYNC_GLOBAL listening for SYNC_LOCAL")
 
-    fmt.Println("[HTTP] VALIDATION listening for SYNC_GLOBAL on " + CONN_HOST + ":" + HTTP_PORT)  
+	fmt.Println("[HTTP] VALIDATION listening for SYNC_GLOBAL on " + CONN_HOST + ":" + port)
 
-    router := mux.NewRouter()
-    router.HandleFunc("/validate/tag", tag).Methods("POST")
-    router.HandleFunc("/validate/card", card).Methods("POST")
-    router.HandleFunc("/validate/contingency", contingency).Methods("POST")
-    http.ListenAndServe(CONN_HOST + ":" + HTTP_PORT, router)
-
+	router := mux.NewRouter()
+	router.HandleFunc("/validate/tag", tag).Methods("POST")
+	router.HandleFunc("/validate/card", card).Methods("POST")
+	router.HandleFunc("/validate/contingency", contingency).Methods("POST")
+	http.ListenAndServe(CONN_HOST+":"+port, router)
 }
 
 func tag(w http.ResponseWriter, r *http.Request) {
 
-    fmt.Println("tag()")
+	fmt.Println("tag()")
 
-    // decrypt
-    // check token
+	// decrypt
+	// check token
 
-    validateTag()// + get vehicle id + vehicle GPS boolean
+	validateTag() // + get vehicle id + vehicle GPS boolean
 
-    validateStation()// + get station Lat/Lon
+	validateStation() // + get station Lat/Lon
 
-    getVehicleLocation()// if vehicle has GPS
+	getVehicleLocation() // if vehicle has GPS
 
-    validateVehicleAtStation()// if vehicle has GPS
+	validateVehicleAtStation() // if vehicle has GPS
 
 }
 
 func card(w http.ResponseWriter, r *http.Request) {
 
-    fmt.Println("card()")
+	fmt.Println("card()")
 
-    // decrypt
-    // check token
+	// decrypt
+	// check token
 
-    validateCard()// + get vehicle id + vehicle GPS boolean
+	validateCard() // + get vehicle id + vehicle GPS boolean
 
-    validateStation()// + get station Lat/Lon
+	validateStation() // + get station Lat/Lon
 
-    getVehicleLocation()// if vehicle has GPS
+	getVehicleLocation() // if vehicle has GPS
 
-    validateVehicleAtStation()// if vehicle has GPS
+	validateVehicleAtStation() // if vehicle has GPS
 
 }
 
 func contingency(w http.ResponseWriter, r *http.Request) {
 
-    fmt.Println("contingency()")
+	fmt.Println("contingency()")
 
 }
 
 func validateTag() {
 
-    fmt.Println("validateTag()")
+	fmt.Println("validateTag()")
 
 }
 
 func validateCard() {
 
-    fmt.Println("validateCard()")
+	fmt.Println("validateCard()")
 
 }
 
 func validateStation() {
 
-    fmt.Println("validateStation()")
+	fmt.Println("validateStation()")
 
 }
 
 func getVehicleLocation() {
 
-    fmt.Println("getVehicleLocation()")
+	fmt.Println("getVehicleLocation()")
 
 }
 
 func validateVehicleAtStation() {
 
-    fmt.Println("validateVehicleAtStation()")
+	fmt.Println("validateVehicleAtStation()")
 
 }
